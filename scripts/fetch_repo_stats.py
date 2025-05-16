c77234c55448425c678cbb1a1c5baee25d828803
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-05-17 01:26:43 +0200

    update file name

--- scripts/fetch_repo_stats.py
+++ scripts/fetch_repo_stats.py
@@ -39,7 +39,7 @@ PROJECTS = [
         "title": "Deep_mind",
         "owner": "comitanigiacomo",
         "repo": "deep_mind",
-        "image": "/About.png",
+        "image": "/About.jpeg",
     }]
 
 HEADERS = {"Accept": "application/vnd.github.v3+json"}
