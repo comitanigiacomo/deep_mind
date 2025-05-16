5b59a3607160d82bb12fb2ac8757391f4a82775a
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-05-16 23:24:57 +0200

    refactor Mynav component to use TypeWriter for branding text

--- src/components/Mynav.jsx
+++ src/components/Mynav.jsx
@@ -1,23 +1,24 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './Mynav.css'
+import './Mynav.css';
+import TypeWriter from './TypeWriter.jsx';
 
 function Mynav() {
   return (
-    <>
-      <Navbar fixed="top" >
-        <Container>
-          <Navbar.Brand href="#about"><span>Deep Mind</span></Navbar.Brand>
-          <Nav className="ms-0">
-            <Nav.Link href="#about"><span>About</span></Nav.Link>
-            <Nav.Link href="#education"><span>Education</span></Nav.Link>
-            <Nav.Link href="#projects"><span>Projects</span></Nav.Link>
-            <Nav.Link href="#contacts"><span>Contacts</span></Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar fixed="top">
+      <Container>
+        <Navbar.Brand href="#about">
+          <TypeWriter text="Deep mind" speed={150} delay={2000} />
+        </Navbar.Brand>
+        <Nav className="ms-0">
+          <Nav.Link href="#about"><span>About</span></Nav.Link>
+          <Nav.Link href="#education"><span>Education</span></Nav.Link>
+          <Nav.Link href="#projects"><span>Projects</span></Nav.Link>
+          <Nav.Link href="#contacts"><span>Contacts</span></Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
