4b8e017b9bbe70aadd5c5e17e7e8e22d737f1143
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-06-19 18:48:39 +0200

    Fix image source for dark mode in About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -72,7 +72,7 @@ export default function About() {
                 {/* Immagine spostata qui */}
                 <div className="image-wrapper">
                   <img 
-                    src={isDarkMode ? "/face2.png" : "/face.png"} 
+                    src={isDarkMode ? "/face2.png" : "/face2.png"} 
                     alt="Profile" 
                     className="about-image" 
                   />
