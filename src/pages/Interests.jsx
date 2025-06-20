9624a9b6cefe243e121dff83d0813d9e3a187d9f
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-06-20 14:56:31 +0200

    Fix image paths in Interests component for correct asset loading

--- src/pages/Interests.jsx
+++ src/pages/Interests.jsx
@@ -16,35 +16,35 @@ export default function Interests() {
       number: "00",
       title: "Formula 1",
       description: "Formula 1 has always fascinated me, i follow every race and enjoy learning about the technology and strategy behind the sport.",
-      image: "../../public/ferrari.png",
+      image: "/ferrari.png",
       skills: ["Race Enthusiast", "Strategic Thinking", "Curiosity for Technology", "Attention to Detail"]
     },
     {
       number: "01",
       title: "IT",
       description: "I'm currently studying IT at university, following a lifelong interest in technology and problem-solving.",
-      image: "../../public/tec.png",
+      image: "/tec.png",
       skills: ["Problem Solving", "Critical Thinking", "Tech Curiosity", "Team Collaboration"]
     },
     {
       number: "02",
       title: "Music",
       description: "I’m passionate about music, I listen to all kinds of genres and enjoy playing the drums in my free time.",
-      image: "../../public/music.png",
+      image: "./music.png",
       skills: ["Rhythm & Timing", "Creativity", "Active Listening", "Musical Expression"]
     },
     {
       number: "03",
       title: "Anime",
       description: "Anime is one of my biggest passions. I watch a wide variety of shows and enjoy the creativity and storytelling behind them.",
-      image: "../../public/luffy.png",
+      image: "/luffy.png",
       skills: ["Anime Enthusiast", "Strong Visual Sense", "Narrative Curiosity", "Consistent Watcher"]
     },
     {
       number: "04",
       title: "Gym",
       description: "Going to the gym has become an important part of my routine. It helps me stay focused, build discipline, and improve both physically and mentally. I enjoy challenging myself and seeing progress over time. I'm currently on a short break due to my studies, but I’ll be getting back to it soon.",
-      image: "../../public/gym.png",
+      image: "/gym.png",
       skills: ["Discipline", "Consistency", "Self-Motivation", "Goal-Oriented"]
     },
   ];
