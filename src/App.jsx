6f390d45f97398a3d2c089ff970236324bc8cf10
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-06-20 19:39:53 +0200

    Refactor App component: pass showIntro and setShowIntro props to About component for better state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import Mynav from './components/Mynav.jsx';
 import About from './pages/About';
 import Experience from './pages/Education';
 import Projects from './pages/Projects';
 import Contacts from './pages/Contacts';
-import Interests from './pages/Interests.jsx'
+import Interests from './pages/Interests.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css';
 import { useEffect } from 'react';
 import VerticalSectionIndicator from './components/VerticalSectionIndicator.jsx';
 
 export default function App() {
+  const [showIntro, setShowIntro] = useState(true);
 
   useEffect(() => {
     if(window.location.hash) {
@@ -30,25 +32,29 @@ export default function App() {
 
   return (
     <ThemeProvider>
-      <Mynav />
-      <VerticalSectionIndicator />
+      {!showIntro && <Mynav />}
+      {!showIntro && <VerticalSectionIndicator />}
       <div className="background-wrapper">
         <div className="container mt-3">
           <section id="about">
-            <About />
+            <About showIntro={showIntro} setShowIntro={setShowIntro} />
           </section>
-          <section id="interests">
-            <Interests />
-          </section>
-          <section id="education">
-            <Experience />
-          </section>
-          <section id="projects">
-            <Projects />
-          </section>
-          <section id="contacts">
-            <Contacts />
-          </section>
+          {!showIntro && (
+            <>
+              <section id="interests">
+                <Interests />
+              </section>
+              <section id="education">
+                <Experience />
+              </section>
+              <section id="projects">
+                <Projects />
+              </section>
+              <section id="contacts">
+                <Contacts />
+              </section>
+            </>
+          )}
         </div>
       </div>
     </ThemeProvider>
